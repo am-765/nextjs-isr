f84c5aded39b20ab4afd291e3d69855b69d1a792
am-765 <zjhghjnlzbghh.765+github@gmail.com>
2021-10-04 22:59:21 +0900

    second commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,10 @@
 export default function Index({ current }: { current: string }) {
-  return <div>現在時刻は{current}です。</div>;
+  return (
+    <>
+      <div>現在時刻を表示予定</div>
+      {/* <div>現在時刻は{current}です。</div> */}
+    </>
+  );
 }
 
 export async function getStaticProps() {
